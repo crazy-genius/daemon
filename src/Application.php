09a9db5125ee43b323b3d1f49e40a3cf9f29b8ff
crazy_genius <crazy_genius@mail.com>
2020-07-25 16:51:38 +1000

    Добавть бесконечный цыкл

--- src/Application.php
+++ src/Application.php
@@ -10,6 +10,18 @@ class Application
 {
     public function run(array $argv, int $argc): int
     {
+        while (true) {
+            $this->loop();
+        }
+
         return ExitCodeEnum::OK;
+    }
+
+    private function loop(): void
+    {
+        $time = (new \DateTime())->format('H:i:s');
+        echo "Current time is: {$time}\n";
+
+        sleep(1);
     }
 }
