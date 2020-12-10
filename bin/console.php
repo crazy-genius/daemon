ec831667f41ea064415c06286d575e01b6a7a879
Minkin <crazy_genius@mail.com>
2020-12-10 09:05:14 +0300

    Rename namespace

--- bin/console.php
+++ bin/console.php
@@ -2,11 +2,20 @@
 
 declare(strict_types=1);
 
-use App\Application;
+use Daemon\Application;
+use Daemon\Runtime\PidStorage;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$application = new Application();
+
+const PID_PATH = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR;//. 'd.pid';
+
+$application = new Application(
+    new PidStorage(
+        PID_PATH,
+        'd'
+    ),
+);
 
 $code = $application->run($argv, $argc);
 
